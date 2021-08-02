6d717fc691e1481d681fdd203666dc249741d450
MattWVile <matt.vile@yahoo.co.uk>
2021-08-02 15:34:42 +0000

    update init to connect to local db

--- make_meal_app/service_1/application/__init__.py
+++ make_meal_app/service_1/application/__init__.py
@@ -1,2 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import getenv
+from . import routes
 import requests
+
+app = Flask(__name__)
+
+app.config.update(
+    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db",
+    SQLALCHEMY_TRACK_MODIFICATINOS=True,
+    SECRET_KEY=str(os.urandom(16))
+)
+
+db = SQLAlchemy(app)
+
+
 api = 'http://localhost:5000'
