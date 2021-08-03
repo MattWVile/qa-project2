750fd5f5e3cfd58401529ab91bfc3051f359b569
MattWVile <matt.vile@yahoo.co.uk>
2021-08-03 15:01:04 +0000

    dockerfiles added

--- make_meal_app/service_4/app.py
+++ make_meal_app/service_4/app.py
@@ -20,6 +20,7 @@ def post_meal():
     main = request.json['main_dish']
     side = request.json['side_dish']
     price = prices['main_dish'][main] + prices['side_dish'][side]
+    
     return jsonify(price)
 
 if __name__ == '__main__':
