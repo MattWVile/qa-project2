8698b3aba2c1778b6709ccbe09697abed7e52a8b
MattWVile <matt.vile@yahoo.co.uk>
2021-08-05 11:09:30 +0000

    mvp fully functional

--- make_meal_app/service_1/application/routes.py
+++ make_meal_app/service_1/application/routes.py
@@ -1,17 +1,17 @@
-from . import db
+from . import app, db
 from .models import Make_meal
-from flask import Flask
-from flask.templating import render_template
-
-app = Flask(__name__)
+from flask import Flask, json
+from flask import render_template
+import requests
 
 @app.route('/')
 def home():
     main_dish = requests.get('http://service-2:5000/get/main_dish').text
-    side_dish = requests.get('http://service-2:5000/get/side_dish').text
+    side_dish = requests.get('http://service-3:5000/get/side_dish').text
     get_total = {'main_dish': main_dish, 'side_dish':side_dish}
     total = requests.post('http://service-4:5000/post/meal', json=get_total).json()
     meal = Make_meal(main_dish=main_dish, side_dish=side_dish, price=total)
     db.session.add(meal)
-    db.commit()
-    return render_template("home.html")
+    db.session.commit()
+    meals = Make_meal.query.all()
+    return render_template("home.html", meals=meals, meal=meal)
