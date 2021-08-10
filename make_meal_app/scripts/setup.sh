1aabd035cec8cad92f9a8498840ea25ba89c9a86
MattWVile <matt.vile@yahoo.co.uk>
2021-08-10 15:10:32 +0000

    fixed setup script

--- make_meal_app/scripts/setup.sh
+++ make_meal_app/scripts/setup.sh
@@ -8,7 +8,9 @@ sudo apt-get install curl jq -y
 if [ -z "$(docker --version 2> /dev/null)" ]; then
     curl https://get.docker.com | sudo bash
     sudo usermod -aG docker $USER
-    curl https://get.docker.com | sudo bash
+fi
+
+if [ -z "$(docker-compose --version 2> /dev/null)" ]; then
     sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
