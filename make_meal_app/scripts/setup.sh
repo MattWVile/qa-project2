51755e852abf11dc233b33deacf8efb40f693b06
MattWVile <matt.vile@yahoo.co.uk>
2021-08-11 08:53:26 +0000

    fixed ansible script

--- make_meal_app/scripts/setup.sh
+++ make_meal_app/scripts/setup.sh
@@ -14,10 +14,7 @@ if [ -z "$(docker --version 2> /dev/null)" ]; then
 fi
 
 if [ -z "$(docker-compose --version 2> /dev/null)" ]; then
-    version=${curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name'}
+    version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
     sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
-
-docker ps > /dev/null 2>&1 \
-    || { echo "Session must be restarted to run Docker commands" >&2 && exit 1; } 
