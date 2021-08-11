7402a598f603f7e882385689ed6ec80aadcc3ca8
MattWVile <matt.vile@yahoo.co.uk>
2021-08-11 08:46:28 +0000

    fixed ansible script

--- make_meal_app/scripts/setup.sh
+++ make_meal_app/scripts/setup.sh
@@ -14,6 +14,7 @@ if [ -z "$(docker --version 2> /dev/null)" ]; then
 fi
 
 if [ -z "$(docker-compose --version 2> /dev/null)" ]; then
+    version=${curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name'}
     sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
