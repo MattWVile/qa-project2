05fa6b5e8bb4fc852be017d45359b8fc730c97bd
MattWVile <matt.vile@yahoo.co.uk>
2021-08-13 09:12:36 +0000

    hidden database_uri

--- make_meal_app/scripts/deploy.sh
+++ make_meal_app/scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
 rsync -r docker-compose.yaml nginx swarm-manager:
+ssh swarm-manager "export DATABASE_URI=${DATABASE_URI}"
 
 ssh swarm-manager << EOF
-    "export DATABASE_URI=${DATABASE_URI} && export SECRET_KEY=${SECRET_KEY} && docker stack deploy --compose-file docker-compose.yaml Password"
     sudo usermod -aG docker $USER 
     docker login -u ${DOCKERHUB_CREDENTIALS_USR} -P ${DOCKERHUB_CREDENTIALS_PSW} 
     docker stack deploy --compose-file docker-compose.yaml make_meal 
